699eb4da234ffa987ab586c22b30d9b340d73b78
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-04-18 14:19:12 -0400

    Python_Scraping de contenidos finalizado

--- 1. Python/cienciasdelsur_scraper.py
+++ 1. Python/cienciasdelsur_scraper.py
@@ -2,8 +2,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Definimos la URL que queremos scrapear y hacemos la petición 
+# Declaración de constantes
 URL = 'https://cienciasdelsur.com/'
+OUTPUT_FILE_ARTICLES_CONTENT = 'EXTRACCION_TEXTOS.txt'
+
+# Definimos la URL que queremos scrapear y hacemos la petición 
 response = requests.get(URL)
 
 # Creamos el objeto BeautifulSoup
@@ -24,10 +27,23 @@ for article in articles_container:
         links_aux.append(link)
 
 # Iteramos sobre la lista de links y sacamos el contenido de cada uno
+all_text_articles = []
 for link in links_articles:
     print("⭕ Iniciando scraping en el articulo: " + link['title'] + "...")
     response = requests.get(link['link'])
     soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Obtenemos el contenido del articulo y extraemos cada parrafo
+    article_content = soup.find('div', {'class': 'td-post-content'}) 
+    article_text_content = article_content.find_all('p')
+    for paragraph in article_text_content:
+        all_text_articles.append(paragraph.text)
+
     print("✅ Articulo: " + link['title'] + " scrapeado con éxito!\n")
 
-    
+# Guardamos el contenido de todos los articulos en un archivo de texto
+with open(OUTPUT_FILE_ARTICLES_CONTENT, 'w', encoding='utf-8') as f:
+    for text in all_text_articles:
+        f.write(text + ' ') 
+
+print("📝 Se ha generado el archivo: " + OUTPUT_FILE_ARTICLES_CONTENT + " con el contenido de todos los articulos scrapeados!")
