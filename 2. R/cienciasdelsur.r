c9a0add7597e8b2e8ef133d2e3023d1c9be8f1c4
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-04-18 15:48:12 -0400

    R - Links de articulos obtenidos

--- 2. R/cienciasdelsur.r
+++ 2. R/cienciasdelsur.r
@@ -6,6 +6,17 @@ URL <- "https://cienciasdelsur.com/"
 # Bloqueamos las "constantes" para evitar reasignaciones
 lockBinding("URL", .GlobalEnv)
 
-# Obtenemos todos los div con la clase 'td-module-thumb'
+# Obtenemos todos los articulos del sitio y extraemos su link
 webpage <- read_html(URL)
 divs <- html_nodes(webpage, ".td-module-thumb")
+
+links <- c()
+for (i in 1:length(divs)) {
+  link <- html_attr(html_node(divs[i], "a"), "href")
+  if (length(which(links == link)) == 0) {
+    links <- c(links, link)
+  }
+}
+links <- unique(links)
+
+print(links)
