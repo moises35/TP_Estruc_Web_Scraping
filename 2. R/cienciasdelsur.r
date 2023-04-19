ddfa777661a247ffb952ef0559d69553b3f03416
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-04-18 20:55:41 -0400

    R - Nube de palabras generadas

--- 2. R/cienciasdelsur.r
+++ 2. R/cienciasdelsur.r
@@ -1,14 +1,24 @@
 library(rvest)
+library(wordcloud)
 
 # Definición de constantes
 URL <- "https://cienciasdelsur.com/"
 OUTPUT_FILE_ARTICLES_CONTENT <- "EXTRACCION_TEXTOS.txt"
 OUTPUT_FILE_FREQUENCIES <- "FRECUENCIAS_PALABRAS.txt"
+WORDS_TO_DISMISS <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", 
+  "r", "s", "t", "u", "v", "y", "x", "w", "z", "y", "o", "no", "el", "la", "los", "las", 
+  "un", "una", "es", "su", "unos", "unas" , "pero", "a", "ante", "bajo", "cabe", "con", "contra", "de", 
+  "desde", "durante", "en", "entre", "hacia", "hasta", "mediante", "para", "por", "según", 
+  "sin", "so", "sobre", "tras", "como", "que", "se", "del", "al", "lo", "como", "esta", 
+  "está", "este", "estos", "estas", "ha", "han", "porque", "si", "sí", "sino", "más", "menos", 
+  "ser", "estar", "fue", "son", "sin", "tal", "también", "todo", "todos", "cual", "cuales", "cuya", "cuyos"
+)
 
 # Bloqueamos las "constantes" para evitar reasignaciones
 lockBinding("URL", .GlobalEnv)
 lockBinding("OUTPUT_FILE_ARTICLES_CONTENT", .GlobalEnv)
 lockBinding("OUTPUT_FILE_FREQUENCIES", .GlobalEnv)
+lockBinding("WORDS_TO_DISMISS", .GlobalEnv)
 
 # Obtenemos todos los articulos del sitio y extraemos su link
 webpage <- read_html(URL)
@@ -45,6 +55,25 @@ write.table(all_text_articles, OUTPUT_FILE_ARTICLES_CONTENT, sep = "\n", row.nam
 file_articles_content <- readLines(OUTPUT_FILE_ARTICLES_CONTENT, encoding = "UTF-8")
 
 # Convertimos todo a minúsculas, eliminamos los caracteres especiales y separamos las palabras
+file_articles_content <- tolower(file_articles_content)
+file_articles_content <- gsub("[^[:alpha:][:space:]ñáéíóúü]", "", file_articles_content)
+words <- unlist(strsplit(file_articles_content, "\\s+"))
+filtered_words <- words[!words %in% WORDS_TO_DISMISS]
+
+# Hallamos la frecuencia, ordenamos y guardamos en el archivo
+word_counts <- table(filtered_words)
+sorted_word_counts <- sort(word_counts, decreasing = TRUE)
+write.table(sorted_word_counts, OUTPUT_FILE_FREQUENCIES, sep = "\t", quote = FALSE)
+
+# Imprimir las 50 primeras frecuencias 
+cat("\nLas 50 palabras más frecuentes son:\n")
+for (i in 1:50) {
+  cat(paste(names(sorted_word_counts)[i], ": ", sorted_word_counts[i], "\n", sep = ""))
+}
 
 
+# ----------------------------------------------------------------------------------
+
+# Creamos la nube de palabras
+wordcloud(words = names(sorted_word_counts)[1:50], freq = sorted_word_counts[1:50], scale = c(5, 0.5), min.freq = 1, max.words = 50, random.order = FALSE, rot.per = 0, colors = brewer.pal(8, "Dark2"))
 
